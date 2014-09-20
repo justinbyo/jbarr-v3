8273c62e62e1209c37d56654c61901435871bcfe
Justin Young <justin.barr.young@gmail.com>
2014-09-20 12:24:42 -0700

    Fixed window padding on every size

--- js/main.js
+++ js/main.js
@@ -5,7 +5,20 @@ function resizeWindow() {
 
 	fullHeightContainer = $('.js-fullHeight');
     windowHeight = $(window).height();
-    fullHeightContainer.css('min-height', windowHeight);
+    // fullHeightContainer.css('min-height', windowHeight);
+    
+    if (windowHeight > 1000 && windowHeight > 800){
+	    $('.js-fullHeight').removeClass('js-mediumPadding');
+	    $('.js-fullHeight').addClass('js-bigPadding');
+    }
+    else if (windowHeight > 800 ){
+	    $('.js-fullHeight').removeClass('js-bigPadding');
+	    $('.js-fullHeight').addClass('js-mediumPadding');
+    }
+    else {
+	    $('.js-fullHeight').removeClass('js-bigPadding');
+	    $('.js-fullHeight').removeClass('js-mediumPadding');
+    }
 }
 
 $(document).ready(resizeWindow);
